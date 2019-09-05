7633025bd73ccf2f2d40d976a37018d2c9a42bdf
luce <luce@ohio.edu>
2019-09-05 15:13:05 -0400

    Add Customer model and controller

---
+++ DAL/SampleContex.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using luce_MIS4200.Models;
+
+namespace luce_MIS4200.DAL
+{
+  
+    public class SampleContext : DbContext //Inherits from DbContext
+    {
+        public SampleContext() : base("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+         //   Database.SetInitializer(new MigrateDatabaseToLatestVersion<SampleContext, luce_MIS4200.Migrations.MISContext.Configuration>("DefaultConnection"));
+        }
+        // Include each object here.  The value inside <> is the name of the class, 
+        // the value outside should generally be the plural of the class name 
+        //    and is the name used to reference the entity in code
+        public DbSet<Customer> Customer { get; set; }
+
+
+        // add this method - it will be used later
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace luce_MIS4200.Models
+{
+    public class Customer
+    {
+        public int customerID { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string phone { get; set; }
+    }
+}
